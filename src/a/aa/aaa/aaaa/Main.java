19998dff648e625b6ba8f2209ce8987b67f78812
HEX191970 <seoultech95@gmail.com>
2023-01-07 00:21:48 +0900

    second step

--- src/a/aa/aaa/aaaa/Main.java
+++ src/a/aa/aaa/aaaa/Main.java
@@ -8,8 +8,14 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		String cmd = sc.nextLine();
-		sc.close();
+		while(true) {
+			System.out.printf("명령어) ");
+			String cmd = sc.nextLine();
+			
+			if(cmd.equals("exit")) {
+				break;
+			}
+		}
 		
 		System.out.println("== 프로그램 끝 ==");
 	}

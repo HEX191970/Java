7ef12baaa6ab8dab3765f68bcf41f12f159fb7fd
HEX191970 <seoultech95@gmail.com>
2023-01-07 00:00:41 +0900

    first step

---
+++ src/a/aa/aaa/aaaa/Main.java
@@ -0,0 +1,16 @@
+package a.aa.aaa.aaaa;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("== 프로그램 시작 ==");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String cmd = sc.nextLine();
+		sc.close();
+		
+		System.out.println("== 프로그램 끝 ==");
+	}
+}
